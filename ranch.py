b35ed2cac829bb5e19ee7505cd41cb7b10ba6ec3
lost-- <belmontypython.cyler@gmail.com>
2015-04-19 08:25:46 -0400

    random quotes work in conjunction with random pics

--- ranch.py
+++ ranch.py
@@ -7,14 +7,19 @@ import random
 
 def ranch_it_up():
     
-    quotes = ['"Hit this ranch"','"Sup Mellow Mike?"','"Hey, hey, hey smoke ranch everyday"','"And on the seventh day he ranched"','"And we know that in all things Ranch works for the good of those who love it, who have been called according to its purpose. - Rom 8:28"','"May the ranch be with you"','"Check it out! Ranch is what its about"','"And the peace of Ranch, which transcends all understanding, will guard your hearts and your minds - Phil 4:7"','"Everybody wants to ranch the world"',
-    '"I dont deserve any ranch for turning the other cheek as my tongue is always in it."','"They say marriages are made in heaven, but so is ranch. - Clint Eastwood"']
-    randQuote = quotes[random.randint(0, len(quotes)-1)]    
+    quotes = ['"Hit this ranch"','"Sup Mellow Mike?"','"Hey, hey, hey smoke ranch everyday"','"And on the seventh day he ranched"','"And we know that in all things Ranch works for the good of those who love it, who have been called according to its purpose." - Rom 8:28','"May the ranch be with you"','"Check it out! Ranch is what its about"','"And the peace of Ranch, which transcends all understanding, will guard your hearts and your minds" - Phil 4:7','"Everybody wants to ranch the world"',
+    '"I dont deserve any ranch for turning the other cheek as my tongue is always in it."','"They say marriages are made in heaven, but so is ranch." - Clint Eastwood']
+    randQuote = quotes[random.randint(0, len(quotes))]    
 
     pic = requests.get("http://api.tumblr.com/v2/tagged?tag=ranch%20dressing&api_key=U74eP3r3GYK3rm94ngtkV2w5csBoHLKriUGpnNh89yApV8VCqB")
     pic = pic.json()
-    randResponse = random.randint(0, len(pic['response'])-1)
-    pic = pic['response'][randResponse]['photos'][0]['original_size']['url']
+
+
+    while(True):
+        randResponse = random.randint(0, len(pic['response']))
+        if(pic['response'][randResponse].has_key('photos')):
+            pic = pic['response'][randResponse]['photos'][0]['original_size']['url']
+            break
 
     with open('access_token.cfg','r') as f:
         access_token = f.read()
@@ -46,8 +51,8 @@ def ranch_it_up():
     print page_token"""
 
     attachment =  {
-    'name': 'Ranch of the Day',
-    'caption': '%s' %(randQuote),
+    'name': '%s' %(randQuote),
+    'caption': '',
     'picture': pic
 	}
 
